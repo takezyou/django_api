08810a636c1dd811155ab168e271888022df04a3
秋田　海人 <kaitoakita07071@gmail.com>
2018-12-16 23:27:29 +0900

    usernameが同じだった時のテストを追加

--- api/tests/signup_test.py
+++ api/tests/signup_test.py
@@ -13,9 +13,8 @@ class SignupTest(APITestCase, URLPatternsTestCase):
     ]
 
     # ユーザー作成テスト
-    def test_create_account(self):
+    def test_create_user_ok(self):
         url = reverse('signup-list')
-        print(url)
         data = {
             'username': 'akita',
             'email': 'test@gmail.com',
@@ -28,3 +27,48 @@ class SignupTest(APITestCase, URLPatternsTestCase):
         self.assertEqual(User.objects.get().email, 'test@gmail.com')
         self.assertEqual(User.objects.get().profile, 'test')
         self.assertTrue(User.objects.get().check_password('akitakaito'))
+
+    # profileはNoneでもよいのでテストしない
+    # usernameが同じものがあるとエラー
+    def test_create_user_username_error(self):
+        url = reverse('signup-list')
+        data = {
+            'username': 'akita',
+            'email': 'test@gmail.com',
+            'profile': 'test',
+            'password': 'akitakaito',
+        }
+        self.client.post(url, data, format='json')
+
+        data = {
+            'username': 'akita',
+            'email': 'akita@gmail.com',
+            'profile': 'akita',
+            'password': 'akitakaito1',
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # emailに同じものがあるとエラー
+    # def test_create_user_email_error(self):
+    #     pass
+
+    '''
+     passwordが8文字以下の場合はエラー
+     passwordと他のusenameなどの類似度が高い場合エラー
+     passwordが数字のみで構成せれていた場合エラー
+    '''
+
+    # def test_create_user_password_error(self):
+    #     url = reverse('signup-list')
+    #     data = {
+    #         'username': 'akita',
+    #         'email': 'test@gmail.com',
+    #         'profile': 'test',
+    #         'password': 'akita',  # 8文字以下にする
+    #     }
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
