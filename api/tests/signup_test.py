672a1f6e12325bfdf822f5829ad494a61c7ceda6
秋田　海人 <kaitoakita07071@gmail.com>
2018-12-16 22:15:09 +0900

    signupの簡単なテストの作成

--- api/tests/signup_test.py
+++ api/tests/signup_test.py
@@ -1,27 +1,30 @@
-from django.urls import include, path, reverse
+from django.urls import include, reverse
+from django.conf.urls import url
 from rest_framework import status
 from rest_framework.test import APITestCase, URLPatternsTestCase
 from django.contrib.auth.hashers import make_password
+from api.urls import router as api
 from api.models import User
 
 
 class SignupTest(APITestCase, URLPatternsTestCase):
     urlpatterns = [
-        path('api/v1/', include('api.urls')),
+        url(r'api/v1/', include(api.urls))
     ]
 
     # ユーザー作成テスト
     def test_create_account(self):
-        url = reverse('signup')
+        url = reverse('signup-list')
+        print(url)
         data = {
             'username': 'akita',
             'email': 'test@gmail.com',
             'profile': 'test',
             'password': 'akitakaito',
         }
-        response = self.client.get(url, data, format='json')
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.get().name, 'akita')
+        self.assertEqual(User.objects.get().username, 'akita')
         self.assertEqual(User.objects.get().email, 'test@gmail.com')
         self.assertEqual(User.objects.get().profile, 'test')
-        self.assertEqual(User.objects.get().password, make_password('akitakaito'))
+        self.assertTrue(User.objects.get().check_password('akitakaito'))
