a3011b1f171a1c3d8e9a9982b24991f887909a7d
秋田　海人 <kaitoakita07071@gmail.com>
2018-12-18 04:05:06 +0900

    140字以上のテストを追加

--- api/tests/post_test.py
+++ api/tests/post_test.py
@@ -238,3 +238,51 @@ class PostUpdateTest(APITestCase, URLPatternsTestCase):
         response = self.client.patch(url + url_id, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # 投稿文が140字以上の時エラー
+    def test_post_update_characters_error(self):
+        url_sigup = reverse('signup-list')
+        url_login = reverse('login-list')
+        url = reverse('post-list')
+
+        data_signup = {
+            'username': 'akita',
+            'email': 'test@gmail.com',
+            'profile': 'test',
+            'password': 'akitakaito',
+        }
+        # ユーザー作成
+        self.client.post(url_sigup, data_signup, format='json')
+
+        data_login = {
+            'email': 'test@gmail.com',
+            'password': 'akitakaito',
+        }
+        # ログイン
+        self.client.post(url_login, data_login, format='json')
+
+        token = Token.objects.get().token
+
+        # 140字以上のランダムな文字列作成
+        body = ''.join(random.choices(string.ascii_letters + string.digits, k=141))
+
+        data = {
+            'body': 'test',
+            'status': 'public',
+        }
+        # 投稿
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post(url, data, format='json')
+
+        url_id = str(Post.objects.get().id) + '/'
+
+        data = {
+            'body': body,
+        }
+        # 投稿を修正
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.patch(url + url_id, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # body_idがなかった時のテストが必要
