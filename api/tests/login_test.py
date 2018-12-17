e87ca2912c6fcfb3f10edb469425de6244d5f8ba
秋田　海人 <kaitoakita07071@gmail.com>
2018-12-17 13:06:54 +0900

    usernameを入力と渡した場合エラーを返す

--- api/tests/login_test.py
+++ api/tests/login_test.py
@@ -34,3 +34,46 @@ class LoginTest(APITestCase, URLPatternsTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    # jsonがない場合のエラー
+    def test_user_login_json_error(self):
+        url_sigup = reverse('signup-list')
+        url = reverse('login-list')
+
+        data_signup = {
+            'username': 'akita',
+            'email': 'test@gmail.com',
+            'profile': 'test',
+            'password': 'akitakaito',
+        }
+        self.client.post(url_sigup, data_signup, format='json')
+
+        response = self.client.post(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # usernameの場合のエラー
+    def test_user_login_username_error(self):
+        url_sigup = reverse('signup-list')
+        url = reverse('login-list')
+
+        data_signup = {
+            'username': 'akita',
+            'email': 'test@gmail.com',
+            'profile': 'test',
+            'password': 'akitakaito',
+        }
+        self.client.post(url_sigup, data_signup, format='json')
+
+        data = {
+            'username': 'akita',
+            'password': 'akitakaito',
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # emailが違う場合エラー
+    def test_user_login_email_error(self):
+        pass
+
