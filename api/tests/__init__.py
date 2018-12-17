88fc74b9835cc10662503863bae593b41206dda1
秋田　海人 <kaitoakita07071@gmail.com>
2018-12-18 02:16:09 +0900

    投稿するテストの作成

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,2 +1,3 @@
 from .signup_test import SignupTest
 from .login_test import LoginTest
+from .post_test import PostCreateTest
--- api/tests/post_test.py
+++ api/tests/post_test.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.urls import include, reverse
+from django.conf.urls import url
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from api.urls import router as api
+from api.models import Token, Post
 
-# Create your tests here.
+
+class PostCreateTest(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+        url(r'api/v1/', include(api.urls))
+    ]
+
+    # 投稿の成功
+    def test_post_create_ok(self):
+        url_sigup = reverse('signup-list')
+        url_login = reverse('login-list')
+        url = reverse('post-list')
+
+        data_signup = {
+            'username': 'akita',
+            'email': 'test@gmail.com',
+            'profile': 'test',
+            'password': 'akitakaito',
+        }
+        # ユーザー作成
+        self.client.post(url_sigup, data_signup, format='json')
+
+        data_login = {
+            'email': 'test@gmail.com',
+            'password': 'akitakaito',
+        }
+        # ログイン
+        self.client.post(url_login, data_login, format='json')
+
+        token = Token.objects.get().token
+
+        data = {
+           'body': 'test',
+           'status': 'public',
+        }
+        # 投稿
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
