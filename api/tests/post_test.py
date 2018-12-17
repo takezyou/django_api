6f244b289c19198f02c6c1037819b27ab459c13a
秋田　海人 <kaitoakita07071@gmail.com>
2018-12-18 02:38:35 +0900

    投稿下書きのテストを追加

--- api/tests/post_test.py
+++ api/tests/post_test.py
@@ -14,7 +14,7 @@ class PostCreateTest(APITestCase, URLPatternsTestCase):
         url(r'api/v1/', include(api.urls))
     ]
 
-    # 投稿の成功
+    # 投稿の成功(public)
     def test_post_create_ok(self):
         url_sigup = reverse('signup-list')
         url_login = reverse('login-list')
@@ -41,6 +41,40 @@ class PostCreateTest(APITestCase, URLPatternsTestCase):
         data = {
            'body': 'test',
            'status': 'public',
+        }
+        # 投稿
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # 投稿下書き(draft)
+    def test_post_create_draft_ok(self):
+        url_sigup = reverse('signup-list')
+        url_login = reverse('login-list')
+        url = reverse('post-list')
+
+        data_signup = {
+            'username': 'akita',
+            'email': 'test@gmail.com',
+            'profile': 'test',
+            'password': 'akitakaito',
+        }
+        # ユーザー作成
+        self.client.post(url_sigup, data_signup, format='json')
+
+        data_login = {
+            'email': 'test@gmail.com',
+            'password': 'akitakaito',
+        }
+        # ログイン
+        self.client.post(url_login, data_login, format='json')
+
+        token = Token.objects.get().token
+
+        data = {
+            'body': 'test',
+            'status': 'draft',
         }
         # 投稿
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
