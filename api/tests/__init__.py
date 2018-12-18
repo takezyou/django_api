054b9d3a834f0f1ef6b8d522f24bb0c33338dbbc
Kaito Akita <kaitokun07@icloud.com>
2018-12-19 05:14:27 +0900

    testsのinitファイルがpushされてなかったのでpushする

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,4 +1,5 @@
 from .signup_test import SignupTest
 from .login_test import LoginTest
+from .logout_test import LogoutTest
 from .post_test import PostCreateTest, PostUpdateTest, PostDeleteTest
 from .post_list_test import PostListTest
