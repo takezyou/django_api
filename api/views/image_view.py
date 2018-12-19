377bdc4a7b607c9e6679a73a790b7d24ab91a869
Kaito Akita <kaitokun07@icloud.com>
2018-12-20 00:48:44 +0900

    urlの設定とbase64で検証

--- api/views/image_view.py
+++ api/views/image_view.py
@@ -2,13 +2,24 @@ from .common_view import CommonView
 from django.http.response import JsonResponse
 
 from api.models import Image
+import base64
 import json
 
 
 class ImageView(CommonView):
 
     def create(self, request):
-        pass
+        try:
+            authorization = self.check_authorization()
+            if authorization:
+                return authorization
+            data = json.loads(request.body)
+            image = data['image']
+        except:
+            # JSONの読み込みに失敗
+            return JsonResponse({'message': 'Post data injustice'}, status=400)
+
+        print(base64.b64decode(image))
 
     def partial_update(self, request, pk=None):
         pass
