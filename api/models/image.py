f4e6ebae218e9e0653dfed128826d2ce972edfdc
Kaito Akita <kaitokun07@icloud.com>
2018-12-21 04:11:41 +0900

    画像uploadの実装　ただ一件取得やレスポンスをどうにかしたい

--- api/models/image.py
+++ api/models/image.py
@@ -1,13 +1,41 @@
 from django.db import models
+from django.core.files.base import ContentFile
+from django.utils import timezone
 from .user import User
+import uuid
+import imghdr
+import json
 
 
 class Image(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    image = models.ImageField(upload_to='shared')
+    image = models.ImageField(upload_to='images', null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
     class Meta:
         db_table = 'images'
+
+    @staticmethod
+    def create(file, user_id):
+
+        date = timezone.now()
+
+        # 12文字で作成
+        file_name = str(uuid.uuid4())[:12]
+
+        extension = imghdr.what(file_name, file)
+        # jpegの時jpgにする
+        extension = "jpg" if extension == "jpeg" else extension
+        # fileの名前を定義
+        complete_file_name = "%s.%s" % (file_name, extension)
+        # ImageFieldに保存するためのobject
+        data = ContentFile(file, name=complete_file_name)
+
+        image = Image.objects.create(user_id=user_id, created_at=date, updated_at=date)
+        image.image.save(complete_file_name, data, save=True)
+        # # レスポンスで返すために1件取得する
+        # image_get = Image.objects.filter(user_id=user_id, image='images/' + complete_file_name)
+
+        return image
--- api/views/image_view.py
+++ api/views/image_view.py
@@ -2,8 +2,9 @@ from .common_view import CommonView
 from django.http.response import JsonResponse
 
 from api.models import Image
-import base64
 import json
+import six
+import base64
 
 
 class ImageView(CommonView):
@@ -14,14 +15,21 @@ class ImageView(CommonView):
             if authorization:
                 return authorization
             data = json.loads(request.body)
+            # base64を受け取る
             image = data['image']
+            # base64で受け取った値を変換してファイルとして保存できるようにする
+            file = base64.b64decode(image)
+            # base64の確認
+            if not isinstance(image, six.string_types):
+                return JsonResponse({'message': 'Does not base64'}, status=400)
         except:
             # JSONの読み込みに失敗
             return JsonResponse({'message': 'Post data injustice'}, status=400)
 
-        print(base64.b64decode(image))
+        # 作成
+        Image.create(file, self.token.user_id)
 
-        return JsonResponse({'message': 'test'}, status=201)
+        return JsonResponse({'result': 'ok'}, status=201)
 
     def partial_update(self, request, pk=None):
         pass
