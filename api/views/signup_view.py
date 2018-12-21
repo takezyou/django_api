01466ba4ad34c93d8f8f2fe25a61388d5faaf7f5
Kaito Akita <kaitokun07@icloud.com>
2018-12-21 22:16:05 +0900

    user_viewの削除とsigupにコメントアウトを追加

--- api/views/signup_view.py
+++ api/views/signup_view.py
@@ -13,25 +13,43 @@ class SignUpView(CommonView):
 
     def create(self, request):
         try:
+            # JSONの読み込み
             data = json.loads(request.body)
         except:
             # JSONの読み込みに失敗
             return JsonResponse({'message': 'Post data injustice'}, status=400)
 
+        # Userの定義
         user = User.objects
+
+        # JSONからusernameの取得
         username = data['username']
+
+        # 同じusernameを持つ人が存在するか判定
         if user.filter(username=username).exists():
             return JsonResponse({'message': 'Duplicate entry username'}, status=400)
 
+        # JSONからemailを取得
         email = data['email']
+
+        # 同じemailが存在するか判定
         if user.filter(email=email).exists():
             return JsonResponse({'message': 'Duplicate entry email'}, status=400)
 
         try:
+            # JSONからpasswordを取得
             password = data['password']
+
+            # AUTH_PASSWORD_VALIDATORSで定義したルールにしたがってバリデーションを行う
             validate_password(password)
+
+            # passwordをpbkdf2の形式に変換
             data['password'] = make_password(password)
+
+            # dataをUserに渡しuserの作成を行う
             user = User.objects.create(**data)
+
+            # 結果をJSON形式で返す
             result = {
                 'id': user.id,
                 'username': user.username,
@@ -41,4 +59,5 @@ class SignUpView(CommonView):
             return JsonResponse(result, status=201)
 
         except ValidationError as e:
+            # バリデーションにかかった時にerror messageと一緒にJSONレスポンスを返す
             return JsonResponse({'message': e.messages[0]}, status=403)
