ab8d433259423664a14eb170ad1bf162d01bfd0b
Kaito Akita <kaitokun07@icloud.com>
2018-12-21 22:23:28 +0900

    post createにコメントアウトを追加

--- api/views/post_view.py
+++ api/views/post_view.py
@@ -9,29 +9,37 @@ class PostView(CommonView):
 
     def create(self, request):
         try:
+            # tokenの確認
             authorization = self.check_authorization()
             if authorization:
+                # tokenがなかったり違っていたらJSONレスポンスを返す
                 return authorization
+
+            # JSONのデータの読み込み
             data = json.loads(request.body)
+
+            # テキストの代入
             body = data['body']
+            # statusの代入
             status = data['status']
         except:
-            # JSONの読み込みに失敗
+            # JSONの読み込みに失敗 keyエラーでも失敗
             return JsonResponse({'message': 'Post data injustice'}, status=400)
 
         # 文字数が140字以内の判定
         if len(body) > 140:
             return JsonResponse({'message': 'Must be 140 characters or less'}, status=403)
 
+        # 投稿(データベースに保存)
         post = Post.create(body, status, self.token.user_id)
 
+        # 結果をdictに保存しJSONレスポンスで返す
         result = {
             'id': post.id,
             'body': post.body,
             'status': post.status,
 
         }
-
         return JsonResponse(result, status=201)
 
     def partial_update(self, request, pk=None):
