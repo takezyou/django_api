347ee00213e619f58e5c8c7be59a013cb57288c6
Kaito Akita <kaitokun07@icloud.com>
2018-12-21 22:37:45 +0900

    post_listにコメントを追加

--- api/views/post_list_view.py
+++ api/views/post_list_view.py
@@ -6,17 +6,23 @@ import json
 
 
 class PostListView(CommonView):
-    def list(self, request):
 
+    # 投稿の一覧
+    def list(self, request):
+        # tokenの確認
         authorization = self.check_authorization()
         if authorization:
+            # tokenがなかったり違っていたらJSONレスポンスを返す
             return authorization
 
+        # 投稿のされている情報の取得(自分以外, 時系列順)
         post_lists = Post.list(self.token.user_id)
 
+        # 取得したlistを1件づつ空のresultに保存し配列で返す
         result = []
         for post_list in post_lists:
             params = {'id': post_list.id, 'body': post_list.body, 'created_at': post_list.created_at}
             result.append(params)
 
-        return JsonResponse(result, safe=False)
+        # safeをFalseにすることで配列を返せるようにしている
+        return JsonResponse(result, safe=False, status=200)
